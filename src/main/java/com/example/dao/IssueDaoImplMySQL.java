c4700eb98d26ca8410a9c6fef829b84ebb70d575
Your Name <you@example.com>
2024-01-09 16:54:09 +0800

    update

--- src/main/java/com/example/dao/IssueDaoImplMySQL.java
+++ src/main/java/com/example/dao/IssueDaoImplMySQL.java
@@ -3,25 +3,21 @@ package com.example.dao;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.example.bean.Issue;
 import com.example.bean.IssueFile;
-import com.example.bean.Project;
-import com.mysql.cj.protocol.Resultset;
 
 @Repository("issuedaomysql")
 public class IssueDaoImplMySQL implements IssueDao {
