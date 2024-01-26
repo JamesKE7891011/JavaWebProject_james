141bcb02a77c96a68cf76ab14df07ab0dc329c97
Your Name <you@example.com>
2024-01-26 09:48:20 +0800

    update

---
+++ src/main/java/com/example/swagger/SwaggerConfig.java
@@ -0,0 +1,51 @@
+package com.example.swagger;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * http://localhost:8080/JavaWebProject_james/mvc/swagger-ui.html
+ */
+@Configuration
+@EnableSwagger2
+@EnableWebMvc
+@ComponentScan(basePackages = "com.example")
+public class SwaggerConfig extends WebMvcConfigurerAdapter {
+
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any())
+				.build()
+				.apiInfo(apiInfo());
+	}
+
+	private ApiInfo apiInfo() {
+		return new ApiInfo(
+				"Rest API", 
+				"Rest API Example", 
+				"1", 
+				"",
+				new Contact("James", "https://github.com/JamesKE7891011/JavaWebProject_james", "james821109@gmail.com"), "Apache License", "");
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+	}
+}
